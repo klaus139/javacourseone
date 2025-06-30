a7353a29e8c9470fd785fdb5d984300892926ae0
klausdev <nickoklaus5@gmail.com>
2025-06-30 11:18:38 +0100

    java var

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String [] args){
+        int age = 32;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19;
+        double temperature = -12.6;
+        double gpa = 3.75;
+
+        char greet = 'H';
+        char symbol = '&';
+        char currency = '$';
+
+        boolean isGood = true;
+
+        String name = "klaus";
+        String food = "rice and beans";
+        String email = "fake123@gmail.com";
+
+
+        System.out.println("hello "+ name + "i like " + food);
+
+
+    }
+}
